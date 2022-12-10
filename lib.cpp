ed1b26fca87e21b31ce6daa78cfe910c53340465
AndreaGenovese03 <118006599+AndreaGenovese03@users.noreply.github.com>
2022-12-10 12:24:06 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,11 @@
+#include "lib.h"
 
+ float maggiore(float numeri[], int dimensione){
+    float max = numeri[0];
+    for (int i=1; i<=dimensione; i++){
+        if (numeri[i]>max){
+            max = numeri[i];
+        }
+    }
+    return max;
+}
