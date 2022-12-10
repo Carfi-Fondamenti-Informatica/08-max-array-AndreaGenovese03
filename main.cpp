ec091c7d32bad1d8ddd5772922387db12ece531b
AndreaGenovese03 <118006599+AndreaGenovese03@users.noreply.github.com>
2022-12-10 12:25:02 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
+using namespace std;
+#include "lib.h"
 
 int main(){
+   int x;
+    cin >> x;
+    float numeri[x];
+
+    for (int i=0; i<x; i++){
+        cin >> numeri[i];
+    }
+
+    cout << maggiore(numeri, x) << endl;
+
   return 0;
 }
